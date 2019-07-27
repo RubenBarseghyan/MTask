4181499897a06f217f0a947f5c113e4f2d5ad760
Ruben <rubenbarseghyan88@gmail.com>
2019-07-27 13:54:28 +0400

    mergin with pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Navigation from './navigation/Navigation';
+import StoryComponent from './components/StoryComponent';
+import ItemComponent from './components/ItemComponent';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1> MarktGuru</h1>
-    </div>
+      <Router>
+          <Navigation/>
+          <div className="container">
+              <h1> MarktGuru</h1>
+              <Switch>
+                  <Route path="/news/:page?" component={StoryComponent} />
+                  <Route path="/item/:item" component={ItemComponent} />
+                  <Route
+                      path="*"
+                      render={() => {
+                          return <Redirect to="/news/1" />;
+                      }}
+                  />
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
