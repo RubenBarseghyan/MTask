ac635a0a6f67e878577b01bee036759c07822034
Ruben <rubenbarseghyan88@gmail.com>
2019-07-27 15:37:40 +0400

    create main importer componentImporter and loader

---
+++ src/assets/Loading.js
@@ -0,0 +1,2 @@
+const loaderImage ='https://yt3.ggpht.com/a/AGF-l7-j3IyVnykBVoYQ1eN2K144VWP2ayWgNqLiLw=s900-mo-c-c0xffffffff-rj-k-no'
+export default loaderImage;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import loaderImage from '../assets/Loading';
+
+export default function mainComponent(importComponent) {
+    class MainComponent extends Component {
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                module: null
+            };
+        }
+
+        async componentDidMount() {
+            const { default: component } = await importComponent();
+            this.setState({module: component});
+        }
+
+        render() {
+            const { module: Component } = this.state;
+            return Component ? <Component {...this.props} /> : <img alt="this is loader" src={loaderImage}/>;
+        }
+    }
+
+    return MainComponent;
+}
+
