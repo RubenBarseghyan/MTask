53bbabd2415e1b61141ff7528ec1d18e9cd1bb22
Ruben <rubenbarseghyan88@gmail.com>
2019-07-27 13:49:15 +0400

    adding pagination component

---
+++ src/pagination/Pagination.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Pagination.css";
+
+const Pagination = (props) => {
+    const { story, page, pages } = props;
+
+    let currentPage = parseInt(page, 10);
+    let nextPage = currentPage !== 1 ? currentPage - 1 : currentPage;
+    let prevPage = currentPage !== pages ? currentPage + 1 : currentPage;
+
+    return (
+        <div className="Pagination">
+            {currentPage !== 1 && <Link to={`/${story}/${nextPage}`}> &#171; prev</Link>}
+
+            <span>{`${currentPage}/${pages}`}</span>
+
+            {currentPage !== pages && (
+                <Link to={`/${story}/${prevPage}`}>next &#187;</Link>
+            )}
+        </div>
+    );
+};
+
+export default Pagination;
+
+
+
